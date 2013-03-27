7a7d825c760a21593ee0005a6fcf04ea0602ac8a
Robin Hack <rhack@redhat.com>
2013-03-27 07:06:40 +0100

    Better error messages. FIX: Return empty array instead undef. Hangling arguments in open.

--- file-blind.pl
+++ file-blind.pl
@@ -14,44 +14,45 @@ sub split_line {
 	$line =~ s/([^\\]?)'/$1/g;
 	my @fields = split (/(?<!\\),/x, $line);
 	if (scalar (@fields) < 4) {
-		return undef;
+		return ();
 	}
-	return undef if ( $fields[0] ne "PROBE" );
+	return () if ( $fields[0] ne "PROBE" );
 	# drop PROBE field
 	shift @fields;
 
 	return @fields;
 }
 
-sub monitor_mode {
+sub get_file_list {
 	my ($argv) = @_;
 
-	open my $stap, "stap ./syscall-monitor.stp -c @$argv|" or die "Can't fork! $!";
+	open my $stap, "stap ./syscall-monitor.stp -c \"@$argv\"|" or die "Can't fork! $!";
 
 	# get list of called syscalls, path names, return codes
 	my @calls = ();
 	while ( my $line = <$stap> ) {
 		my @call = split_line ($line);
-		if ( ! @call ) {
+		if ( !@call ) {
 			# is program line? just print line
 			print $line;
 			next;
 		}
-
 		push @calls, \@call;
 	}
-	close $stap or die "Failed! I can't run program as param!";
+	close $stap or die "FAIL! I can't execute program!";
 
 	return @calls;
-}
-
-sub inject_mode {
 }
 
 sub main {
 	my ($argv) = @_;
 
-	my @calls = monitor_mode ($argv);
+	my @calls = get_file_list ($argv);
+
+	if ( ! @calls ) {
+		print "Nothing to examine.\n";
+		return 0;
+	}
 
 	return 0;
 }
